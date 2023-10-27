3465ec4c9460da02490b3e04748c5cddb5d3b44c
Masahiro Sakamoto <massakam@yahoo-corp.jp>
2023-10-27 19:37:07 +0900

    Introduce hawkeye to check license headers

--- build-support/generate-source-archive.sh
+++ build-support/generate-source-archive.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,7 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-#
+
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 VERSION=$(npm version | grep pulsar-client | awk -F ':' '{print $2}' | sed "s?'??g" | sed "s?,??g" | sed "s? ??g")
